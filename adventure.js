4c27dbc1b2c26141d853d93265bc10c831013389
Dylan Marvel <marvel.dylan.james@gmail.com>
2022-03-03 15:12:57 -0600

    created Pizza Rat using Enemy class instance

--- adventure.js
+++ adventure.js
@@ -22,7 +22,7 @@ class Hero {
     }
 
     announceHealth() {
-        console.log(`My current health is ${this.health}!`);
+        console.log(`${this.name}: My current health is ${this.health}!`);
     }
 
     battle() {
@@ -30,4 +30,39 @@ class Hero {
     }
 }
 
-const DougieDonut = new Hero("Dougie the Donut");
+let DougieDonut = new Hero("Dougie the Donut");
+
+
+class Enemy {
+    constructor(name) {
+        this.name = name;
+        this.health = 100;
+        this.objects = {
+            pepperoniStars: 5,
+            cheeseGrease: 10    
+        };
+        this.catchPhrases = ['i\'m youtube famous',
+        'i\'m more dangerous than an uncovered sewer'];
+    }
+
+    talkSmack() {
+        let tossUp = Math.random();
+
+        if(tossUp > 0.5) {
+           console.log(this.catchPhrases[0]);
+        } else {
+            console.log(this.catchPhrases[1]);
+        }
+
+    }
+
+    announceHealth() {
+        console.log(`${this.name}: My current health is ${this.health}!`);
+    }
+
+    battle() {
+        console.log('I\'m gonna flatten you like a slice of pepperoni!');
+    }
+}
+
+let PizzaRat = new Enemy ('Pizza Rat');
