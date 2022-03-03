fc19292dc08917fac989cb01c560cbd4eb880d89
Dylan Marvel <marvel.dylan.james@gmail.com>
2022-03-03 14:56:31 -0600

    created Timmys Story

--- objects.js
+++ objects.js
@@ -33,7 +33,7 @@ class Person {
         return this.name;
     }
     getAge () {
-        return this.age;
+        console.log(this.age);
     }
     getWeight () {
         return this.weight;
@@ -61,3 +61,50 @@ class Person {
     }
 
 }
+
+// Instantiate a new Person named Timmy
+let Timmy = new Person('Timmy');
+
+// Age Timmy five years
+for(let i = 0; i < 5; i++){
+    Timmy.ageUp();
+}
+
+
+// At this point Timmy's a little bummed. As a precocious child, he feels he's "seen it all" already. Have him eat five times.
+for(let i = 0; i < 5; i++){
+    Timmy.eat();
+}
+// Now Timmy's a little heavier than he wants to be. Kindergarten's coming up and he wants to feel healthier before starting. Have him exercise five times
+for(let i = 0; i < 5; i++){
+    Timmy.exercise();
+}
+// Age Timmy 9 years
+for(let i = 0; i < 9; i++){
+    Timmy.ageUp();
+}
+// Create a hamster named "Gus"
+let Gus = new Hamster('Gus'); 
+
+// Set Gus's owner to the string "Timmy"
+Gus.owner = "Timmy";
+
+// Have Timmy "buy" Gus
+Timmy.buyHamster(Gus);
+
+// Age Timmy 15 years
+for(let i = 0; i < 15; i++){
+    Timmy.ageUp();
+}
+
+// Have Timmy eat twice
+for(let i = 0; i < 2; i++){
+    Timmy.eat();
+}
+// Have Timmy exercise twice
+for(let i = 0; i < 2; i++){
+    Timmy.exercise();
+}
+
+console.log(Timmy);
+console.log(Gus);
